0e5ccacc465d306336a66a66cd74f8fd62ff8dd1
Sina Kashi Pazha <esterlinkof@gmail.com>
2019-11-29 13:13:04 +0330

    Test com.jcabi.ssh package succesfully.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
 
+import com.jcabi.ssh.Shell;
+import com.jcabi.ssh.Ssh;
+
 public class Main {
     private static String readPrivateKey() throws IOException {
         File file = new File("./keys/id_rsa");
@@ -9,13 +12,15 @@ public class Main {
             StringBuilder key = new StringBuilder();
 
             while ((st = br.readLine()) != null)
-                key.append(st);
+                key.append(st).append("\n");
 
             return key.toString();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(readPrivateKey());
+        Shell shell = new Ssh("kashipazha.ir", 22, "asa", readPrivateKey());
+        String stdout = new Shell.Plain(shell).exec("echo 'Hello, world!'");
+        System.out.println(stdout);
     }
 }
